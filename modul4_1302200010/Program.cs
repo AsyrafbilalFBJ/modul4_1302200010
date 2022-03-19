d83b83d895d4e9cb265e026adb623aa7fa07b333
AsyrafbilalFBJ <asyrafbilalrpltelu@student.telkomuniversity.ac.id>
2022-03-19 11:28:24 +0700

    menambahkan class SimpleDataBase

--- modul4_1302200010/Program.cs
+++ modul4_1302200010/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace modul4_1302200010
 {
+    class Penjumlahan
+    {
+        public static void JumlahTigaAngka<T>(T A1, T A2, T A3)
+        {
+            dynamic a1 = A1;
+            dynamic a2 = A2;
+            dynamic a3 = A3;
+            Console.WriteLine(a1 + a2 + a3);
+        }
+        
+    }
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+        public void AddNewData(T dataBaru, DateTime dataDate)
+        {
+            storedData.Add(dataBaru);
+            inputDates.Add(dataDate);
+        }
+        public void PrintAllData()
+        {
+            foreach (T Data in storedData)
+            {
+                Console.WriteLine(Data);
+            }
+            foreach (DateTime DataDate in inputDates)
+            {
+                Console.WriteLine(DataDate);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Penjumlahan.JumlahTigaAngka<long>(13, 02, 20);
+            SimpleDataBase<int> objData = new SimpleDataBase<int>();
+            objData.AddNewData(12, DateTime.Now);
+            objData.AddNewData(34, DateTime.Now);
+            objData.AddNewData(56, DateTime.Now);
+            objData.PrintAllData();
+            Console.ReadKey();
         }
     }
 }
