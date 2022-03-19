cea62740203518261b3e4016bfe2c7a71d01f714
AsyrafbilalFBJ <asyrafbilalrpltelu@student.telkomuniversity.ac.id>
2022-03-19 09:50:21 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

--- modul4_1302200010/Program.cs
+++ modul4_1302200010/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace modul4_1302200010
 {
+    class Penjumlahan
+    {
+        public static void JumlahTigaAngka<T>(T A1, T A2, T A3)
+        {
+            dynamic a1 = A1;
+            dynamic a2 = A2;
+            dynamic a3 = A3;
+            Console.WriteLine(a1 + a2 + a3);
+        }
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Penjumlahan.JumlahTigaAngka<long>(13, 02, 20);
+            Console.ReadKey();
         }
     }
 }
